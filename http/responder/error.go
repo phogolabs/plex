7536aa04c1b21b5ff42ebf2270aa2b0f2001dee1
Svetlin Ralchev <svet@ralch.com>
2020-03-20 16:08:27 +0000

    Rename ErrorxHandler to PostgreSQLFormatter

--- http/responder/error.go
+++ http/responder/error.go
@@ -10,8 +10,8 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// ErrorxHandler can be used to handle an error as general proto message defined by gRPC.
-func ErrorxHandler(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+// PostgreSQLFormatter can be used to handle an error as general proto message defined by gRPC.
+func PostgreSQLFormatter(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
 	statusErr, ok := status.FromError(err)
 	if !ok {
 		statusErr = status.New(codes.Unknown, err.Error())
