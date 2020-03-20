52310de038b8aca466464560bf18b3a63aefaa34
Svetlin Ralchev <svet@ralch.com>
2020-03-20 20:36:10 +0000

    Refine the API responder

--- http/responder/error.go
+++ http/responder/error.go
@@ -10,8 +10,8 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// PostgreSQLFormatter can be used to handle an error as general proto message defined by gRPC.
-func PostgreSQLFormatter(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
+// PostgreSQLErrorFormatter can be used to handle an error as general proto message defined by gRPC.
+func PostgreSQLErrorFormatter(ctx context.Context, mux *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, r *http.Request, err error) {
 	statusErr, ok := status.FromError(err)
 	if !ok {
 		statusErr = status.New(codes.Unknown, err.Error())
