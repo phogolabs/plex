8433bdc3b02d9ba23cb51f707fed982a0570093c
Svetlin Ralchev <svet@ralch.com>
2020-04-07 10:10:35 +0100

    Improve error logging

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/phogolabs/plex
 go 1.13
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/go-chi/chi v4.0.3+incompatible
 	github.com/go-playground/validator/v10 v10.2.0
 	github.com/golang/protobuf v1.3.3
@@ -14,5 +15,6 @@ require (
 	github.com/phogolabs/restify v0.0.0-20200317091006-2808c050cdc3
 	github.com/soheilhy/cmux v0.1.4
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
+	google.golang.org/genproto v0.0.0-20200406120821-33397c535dc2
 	google.golang.org/grpc v1.28.0
 )
--- grpc/interceptor/validator.go
+++ grpc/interceptor/validator.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	validate "github.com/go-playground/validator/v10"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -31,15 +32,34 @@ func (h *ValidationHandler) Unary(ctx context.Context, req interface{}, info *gr
 		err = h.validator().StructCtx(ctx, req)
 	}
 
-	if err != nil {
-		if _, ok := status.FromError(err); !ok {
-			err = status.Error(codes.InvalidArgument, err.Error())
-		}
-
-		return nil, err
+	if err == nil {
+		return handler(ctx, req)
 	}
 
-	return handler(ctx, req)
+	state, ok := status.FromError(err)
+	if !ok {
+		err = status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	switch errx := err.(type) {
+	case validate.ValidationErrors:
+		badRequest := &errdetails.BadRequest{}
+
+		for _, errf := range errx {
+			if errd, ok := errf.(error); ok {
+				badRequest.FieldViolations = append(badRequest.FieldViolations,
+					&errdetails.BadRequest_FieldViolation{
+						Field:       errf.Field(),
+						Description: errd.Error(),
+					},
+				)
+			}
+		}
+
+		state, _ = state.WithDetails(badRequest)
+	}
+
+	return nil, state.Err()
 }
 
 // Stream does not validate the stream
