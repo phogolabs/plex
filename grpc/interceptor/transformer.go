c1e256858f5c68b561cf4bd241df1ede9b14bd6e
Svetlin Ralchev <svet@ralch.com>
2020-06-22 18:11:46 +0100

    Rename the transformer to molder

--- grpc/interceptor/transformer.go
+++ grpc/interceptor/transformer.go
@@ -10,31 +10,19 @@ import (
 // Transformer is the interceptor that transforms the values of each input and
 // output parameter
 var Transformer = &TransformHandler{
-	Transformer: mold.New(),
+	Molder: mold.New(),
 }
 
 // TransformHandler represents a mold
 type TransformHandler struct {
-	Transformer *mold.Transformer
-}
-
-// Register adds a transformation with the given tag
-func (h *TransformHandler) Register(tag string, fn mold.Func) {
-	h.Transformer.Register(tag, fn)
-}
-
-// RegisterStruct registers a StructLevelFunc against a number of types.
-// Why does this exist? For structs for which you may not have access or rights to add tags too,
-// from other packages your using.
-func (h *TransformHandler) RegisterStruct(fn mold.StructLevelFunc, types ...interface{}) {
-	h.Transformer.RegisterStructLevel(fn, types...)
+	Molder *mold.Transformer
 }
 
 // Unary does unary validation
 func (h *TransformHandler) Unary(ctx context.Context, input interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	// transform input
 	if input != nil {
-		if err := h.Transformer.Struct(ctx, input); err != nil {
+		if err := h.Molder.Struct(ctx, input); err != nil {
 			return nil, err
 		}
 	}
@@ -44,7 +32,7 @@ func (h *TransformHandler) Unary(ctx context.Context, input interface{}, info *g
 
 	// transform output
 	if output != nil {
-		if err := h.Transformer.Struct(ctx, output); err != nil {
+		if err := h.Molder.Struct(ctx, output); err != nil {
 			return nil, err
 		}
 	}
